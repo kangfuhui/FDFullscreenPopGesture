7b4e262384c6313c355aba2fe441a023d9f1fdad
Anonymous <Anonymous@163.com>
2016-08-29 17:17:11 +0800

    取消viewWillAppear 的swizzledMethod

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
@@ -128,23 +128,23 @@ typedef void (^_FDViewControllerWillAppearInjectBlock)(UIViewController *viewCon
 
 + (void)load
 {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(viewWillAppear:);
-        SEL swizzledSelector = @selector(fd_viewWillAppear:);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        Class class = [self class];
+//        
+//        SEL originalSelector = @selector(viewWillAppear:);
+//        SEL swizzledSelector = @selector(fd_viewWillAppear:);
+//        
+//        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+//        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+//        
+//        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+//        if (success) {
+//            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+//        } else {
+//            method_exchangeImplementations(originalMethod, swizzledMethod);
+//        }
+//    });
 }
 
 - (void)fd_viewWillAppear:(BOOL)animated
